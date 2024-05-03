e6dbccb3e2fc598fded17b2e71af73d623c315ac
Szymon Miksza <135223619+sszymonm@users.noreply.github.com>
2024-05-03 17:38:20 +0200

    better code splitting
    added class contains elements of game

--- main.py
+++ main.py
@@ -3,16 +3,111 @@
 # from random import randint, choice
 from enemies import *
 
+class Game:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((1280, 720))
+        self.player = Square_player(500, 400, 40, 40, "white")
+        self.GameManager = GameManager(self.screen, self.player)
+        self.running = False
+        self.pause = False
+        self.reset = True
+
+        start_font = pygame.font.Font(None, 100)
+        self.start_text = start_font.render("Square Game", True, (255, 255, 255))
+
+        start_font = pygame.font.Font(None, 100)
+        self.start_text = start_font.render("Square Game", True, (255, 255, 255))
+
+        instruction_font = pygame.font.Font(None, 50)
+        self.instruction_text_start = instruction_font.render(
+            "Press space to start game", True, (255, 255, 255)
+        )
+
+        self.text_the_end = instruction_font.render(
+            "Press space to play again", True, (255, 255, 255)
+        )
+
+        self.instruction_text_reset = instruction_font.render(
+            "Press q to reset game", True, (255, 255, 255)
+        )
+
+        self.instruction_text_pause = instruction_font.render(
+            "Press p to resume game", True, (255, 255, 255)
+        )
+
+        pygame.init()
+        # screen = pygame.display.set_mode((1280, 720))
+        pygame.display.set_caption("Square game")
+
+        clock = pygame.time.Clock()
+
+    def loop(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.running = True
+                elif event.key == pygame.K_q and self.pause == True:
+                    self.GameManager.clear()
+                    self.GameManager.score = 0
+                    self.GameManager.start_game(self.player)
+                    self.running = True
+                elif event.key == pygame.K_p:
+                    if self.running == True:
+                        self.running = False
+                        self.pause = True
+                    else:
+                        self.running = True
+                        self.pause = False
+
+        if self.running:
+            screen.fill((94, 129, 162))
+
+            self.player.update()
+            self.GameManager.update()
+
+            if self.GameManager.collision_enemy():
+                self.GameManager.first_game = False
+                self.reset = True
+                self.running = False
+
+            self.GameManager.take_money()
+
+        else:
+            self.screen.fill((0, 110, 84))
+            screen.blit(self.start_text, (400, 50))
+
+            if self.pause:
+                screen.blit(self.instruction_text_pause, (425, 600))
+                screen.blit(self.instruction_text_reset, (450, 650))
+            else:
+                if self.GameManager.first_game:
+                    screen.blit(self.instruction_text_start, (425, 600))
+                else:
+                    screen.blit(self.text_the_end, (425, 600))
+
+                if self.reset == True:
+                    self.GameManager.clear()
+                    self.GameManager.start_game(self.player)
+                    reset = False
+
+        pygame.display.flip()
+        clock.tick(60)
+
 
 
 class GameManager:
-    def __init__(self):
+    def __init__(self,screen,player):
         self.square_bots = []
         self.moneys = []
         self.score = 0
         self.font_score = pygame.font.Font(None, 50)
         self.first_game = True
         self.timer_points = 0
+        self.screen=screen
+        self.player=player
 
     def generate_enemy(self):
         # choose which type enemy to generate
@@ -91,106 +186,27 @@ class GameManager:
 
     def collision_enemy(self):
         for i in range(0, len(self.square_bots)):
-            if (player.get_pos()).colliderect(self.square_bots[i].get_pos()):
+            if (self.player.get_pos()).colliderect(self.square_bots[i].get_pos()):
                 self.score = 0
                 return True
 
     def take_money(self):
         for i in range(0, len(self.moneys)):
-            if (player.get_pos()).colliderect(self.moneys[i].get_pos()):
+            if (self.player.get_pos()).colliderect(self.moneys[i].get_pos()):
                 del self.moneys[i]
                 self.score += 1
                 return True
 
 
 pygame.init()
-# screen = pygame.display.set_mode((1280, 720))
-pygame.display.set_caption("Square game")
-
+        
+        
 clock = pygame.time.Clock()
-running = False
-pause = False
+Game = Game()
 
-player = Square_player(500, 400, 40, 40, "white")
-
-Game = GameManager()
-reset = True
-
-start_font = pygame.font.Font(None, 100)
-start_text = start_font.render("Square Game", True, (255, 255, 255))
-
-instruction_font = pygame.font.Font(None, 50)
-instruction_text_start = instruction_font.render(
-    "Press space to start game", True, (255, 255, 255)
-)
-
-text_the_end = instruction_font.render(
-    "Press space to play again", True, (255, 255, 255)
-)
-
-instruction_text_reset = instruction_font.render(
-    "Press q to reset game", True, (255, 255, 255)
-)
-
-instruction_text_pause = instruction_font.render(
-    "Press p to resume game", True, (255, 255, 255)
-)
 
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                running = True
-            elif event.key == pygame.K_q and pause == True:
-                Game.clear()
-                Game.score = 0
-                Game.start_game(player)
-                running = True
-            elif event.key == pygame.K_p:
-                if running == True:
-                    running = False
-                    pause = True
-                else:
-                    running = True
-                    pause = False
-
-    if running:
-        screen.fill((94, 129, 162))
-
-        player.update()
-        Game.update()
-
-        if Game.collision_enemy():
-            Game.first_game = False
-            reset = True
-            running = False
-
-        Game.take_money()
-
-    else:
-        screen.fill((0, 110, 84))
-        screen.blit(start_text, (400, 50))
-
-        if pause:
-            screen.blit(instruction_text_pause, (425, 600))
-            screen.blit(instruction_text_reset, (450, 650))
-        else:
-            if Game.first_game:
-                screen.blit(instruction_text_start, (425, 600))
-            else:
-                screen.blit(text_the_end, (425, 600))
-
-            if reset == True:
-                Game.clear()
-
-                Game.start_game(player)
-                reset = False
-
-    pygame.display.flip()
-    clock.tick(60)
+    Game.loop()
 
 
 pygame.quit()
